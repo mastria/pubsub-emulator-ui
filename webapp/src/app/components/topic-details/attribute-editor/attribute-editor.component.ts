bcc02a73a3d21eb960606ddf467a8a5248329641
Guilherme Mastria <guilherme@mastria.dev.br>
2026-04-29 18:49:33 -0300

    feat: persist message attributes per topic with JSON editing

--- webapp/src/app/components/topic-details/attribute-editor/attribute-editor.component.ts
+++ webapp/src/app/components/topic-details/attribute-editor/attribute-editor.component.ts
@@ -25,9 +25,12 @@ import { MatFormField, MatInput } from '@angular/material/input';
   styleUrl: './attribute-editor.component.scss'
 })
 export class AttributeEditorComponent {
-  public attributes: { [key: string]: string } = inject(MAT_DIALOG_DATA).attributes
+  public attributes: { [key: string]: string } = { ...inject(MAT_DIALOG_DATA).attributes }
   newKeyControl = new FormControl<string>("", Validators.required)
   newValueControl = new FormControl<string>("", Validators.required)
+  jsonMode = false
+  jsonInput = new FormControl<string>("")
+  jsonError = false
 
   constructor(
     private dialogRef: MatDialogRef<AttributeEditorComponent>
@@ -49,11 +52,55 @@ export class AttributeEditorComponent {
     }
   }
 
+  toggleJsonMode() {
+    if (!this.jsonMode) {
+      // Entering JSON mode: serialize current attributes
+      this.jsonInput.setValue(JSON.stringify(this.attributes, null, 2))
+      this.jsonError = false
+      this.jsonMode = true
+    } else {
+      // Leaving JSON mode: try to parse
+      if (this.applyJson()) {
+        this.jsonMode = false
+      }
+    }
+  }
+
+  private applyJson(): boolean {
+    try {
+      const parsed = JSON.parse(this.jsonInput.value ?? '{}')
+      if (typeof parsed !== 'object' || Array.isArray(parsed)) {
+        this.jsonError = true
+        return false
+      }
+      for (const val of Object.values(parsed)) {
+        if (typeof val !== 'string') {
+          this.jsonError = true
+          return false
+        }
+      }
+      this.attributes = parsed
+      this.jsonError = false
+      return true
+    } catch {
+      this.jsonError = true
+      return false
+    }
+  }
+
+  clearAll() {
+    this.attributes = {}
+    this.jsonInput.setValue('{}')
+  }
+
   discardChanges() {
     this.dialogRef.close()
   }
 
   saveChanges() {
+    if (this.jsonMode) {
+      if (!this.applyJson()) return
+    }
     this.dialogRef.close(this.attributes)
   }
 }
--- webapp/src/app/components/topic-details/topic-details.component.ts
+++ webapp/src/app/components/topic-details/topic-details.component.ts
@@ -1,5 +1,5 @@
 import { CdkTextareaAutosize } from '@angular/cdk/text-field';
-import { Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { ReactiveFormsModule, UntypedFormControl, Validators } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
@@ -9,6 +9,10 @@ import { MatInput } from '@angular/material/input';
 import { Topic } from 'src/app/services/pubsub.service';
 import { AttributeEditorComponent } from './attribute-editor/attribute-editor.component';
 
+function attributesStorageKey(topicName: string): string {
+  return `message-attributes:${topicName}`
+}
+
 @Component({
   selector: 'app-topic-details',
   templateUrl: './topic-details.component.html',
@@ -16,7 +20,7 @@ import { AttributeEditorComponent } from './attribute-editor/attribute-editor.co
   standalone: true,
   imports: [MatButton, MatIcon, MatFormField, MatInput, CdkTextareaAutosize, ReactiveFormsModule]
 })
-export class TopicDetailsComponent implements OnInit {
+export class TopicDetailsComponent implements OnInit, OnChanges {
 
   @Input() topic?: Topic
   @Output() onMessagePublish = new EventEmitter<{ topic: Topic, message: string, attributes: { [key: string]: string } }>()
@@ -28,26 +32,52 @@ export class TopicDetailsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.loadAttributes()
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['topic'] && !changes['topic'].firstChange) {
+      this.loadAttributes()
+    }
+  }
+
+  private loadAttributes(): void {
+    this.attributes = {}
+    this.attributeCount = 0
+    if (!this.topic) return
+    const stored = localStorage.getItem(attributesStorageKey(this.topic.name))
+    if (stored) {
+      try {
+        this.attributes = JSON.parse(stored)
+        this.attributeCount = Object.keys(this.attributes).length
+      } catch (e) {
+        console.error('Failed to parse stored attributes for topic', this.topic?.name, e)
+      }
+    }
   }
 
   editAttributes() {
-    let dialogRef = this._dialog.open(AttributeEditorComponent, { data: { attributes: this.attributes } })
+    let dialogRef = this._dialog.open(AttributeEditorComponent, { data: { attributes: { ...this.attributes } } })
 
     dialogRef.afterClosed().subscribe(result => {
-      if (result) {
+      if (result !== undefined) {
         this.attributes = result
         this.attributeCount = Object.keys(this.attributes).length
+        localStorage.setItem(attributesStorageKey(this.topic!.name), JSON.stringify(this.attributes))
       }
     })
   }
 
-  publishMessage() {
-    console.log("this value was found", this.inputField.value)
-
-    this.onMessagePublish.emit({ topic: this.topic!, message: this.inputField.value, attributes: this.attributes })
-    this.inputField.reset()
+  clearAttributes() {
     this.attributes = {}
     this.attributeCount = 0
+    localStorage.removeItem(attributesStorageKey(this.topic!.name))
+  }
+
+  publishMessage() {
+    console.log("this value was found", this.inputField.value)
+    this.onMessagePublish.emit({ topic: this.topic!, message: this.inputField.value, attributes: this.attributes })
+    this.inputField.reset()
   }
 
 }
